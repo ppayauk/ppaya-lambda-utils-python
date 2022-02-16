6c55bfd36a4253179cf24800f8642b89579da044
Paul Cooney <p@ulcooney.co.uk>
2022-02-16 17:18:32 +0000

    Helper function for express workflow step functions.

--- ppaya_lambda_utils/exceptions.py
+++ ppaya_lambda_utils/exceptions.py
@@ -1,3 +1,13 @@
 
 class InvokeLambdaFunctionException(Exception):
     """ Raised when the invocation of a lamda function fails """
+
+
+class WorkflowException(Exception):
+    """ Raised when a workflow / state machine fails """
+    def __init__(self, msg: str, error_type: str) -> None:
+        self.msg = msg
+        self.error_type = error_type
+
+    def __str__(self):
+        return f'{self.error_type}: {self.msg}'
